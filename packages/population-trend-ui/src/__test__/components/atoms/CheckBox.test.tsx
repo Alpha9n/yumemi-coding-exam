8696a080ab9142d5a7f73044562b05d1699bdf57
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-26 18:06:06 +0900

    test: 不足属性の追加

--- packages/population-trend-ui/src/__test__/components/atoms/CheckBox.test.tsx
+++ packages/population-trend-ui/src/__test__/components/atoms/CheckBox.test.tsx
@@ -6,22 +6,24 @@ import { setup } from '../../utils/testUtils';
 import React from 'react';
 
 describe('checkBox ロジックテスト', () => {
+    const setState = vi.fn((state: boolean) => {return state});
+    const labelText = 'hoge';
+
     test('初期状態', async () => {
-        setup(<CheckBox label="hoge"/>);
+        setup(<CheckBox onChange={setState} checked={false} label={labelText} id="hoge" name="hoge"/>);
         const target = await screen.findByRole('checkbox');
         expect(target).not.toBeChecked(); // 初期状態
     });
 
     test('クリック時チェックがつく', async () => {
-        const { user } = setup(<CheckBox label="hoge"/>);
+        const { user } = setup(<CheckBox onChange={setState} checked={false} label={labelText} id="hoge" name="hoge"/>);
         const target = await screen.findByRole('checkbox');
         await user.click(target);
         expect(target).toBeChecked();
     });
 
     test('ラベルクリック時チェックがつく', async () => {
-        const labelText = 'hoge';
-        const { user } = setup(<CheckBox label={labelText}/>);
+        const { user } = setup(<CheckBox onChange={setState} checked={false} label={labelText} id="hoge" name="hoge"/>);
         const target = await screen.findByText(labelText);
         await user.click(target);
         const checkBox = await screen.findByRole('checkbox');
@@ -29,8 +31,7 @@ describe('checkBox ロジックテスト', () => {
     });
 
     test('クリック時に渡されたステートを変更する', async () => {
-        const setState = vi.fn((state: boolean) => {return state});
-        const { user } = setup(<CheckBox label="hoge" onClick={setState}/>);
+        const { user } = setup(<CheckBox checked={false} onChange={setState} label={labelText}  id="hoge" name="hoge"/>);
         const target = await screen.findByRole('checkbox');
         await user.click(target);
         expect(setState).toHaveReturnedWith(true);
@@ -38,15 +39,18 @@ describe('checkBox ロジックテスト', () => {
 });
 
 describe('checkbox 外観テスト', () => {
+    const setState = vi.fn((state: boolean) => {return state});
+    const labelText = 'hoge';
+    
     test('初期状態', async () => {
-        setup(<CheckBox label="hoge"/>);
+        setup(<CheckBox onChange={setState} checked={false} label={labelText} id="hoge" name="hoge"/>);
         const target = await screen.findByRole('checkbox');
         expect(target).toHaveStyle({ backgroundColor: '#ffffff' });
     });
 
     test('クリック時色合いが反転する', async () => {
         const labelText = 'hoge';
-        const { user } = setup(<CheckBox label={labelText}/>);
+        const { user } = setup(<CheckBox onChange={setState} checked={false} label={labelText} id="hoge" name="hoge"/>);
         const target = await screen.findByText(labelText);
         await user.click(target);
         const checkBox = await screen.findByRole('checkbox');
