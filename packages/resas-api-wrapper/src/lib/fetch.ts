f3f3405701e5bddfa45bde7995ffaf408ca0dc67
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-29 04:45:14 +0900

    update: apiKeyを外部から引数として取得するよう変更

--- packages/resas-api-wrapper/src/lib/fetch.ts
+++ packages/resas-api-wrapper/src/lib/fetch.ts
@@ -2,22 +2,37 @@ import { createUrl } from '../utilities/endpointUtil';
 import envUtil from '../utilities/envUtil';
 import { PopulationResponseObject, populationResponseObjectSchema, PrefectureResponseObject, prefectureResponseObjectSchema } from './types';
 
-const header = new Headers();
-header.append('X-API-KEY', envUtil.apiKey);
-header.append('content-type', 'application/json');
-
-const requestOptions: RequestInit = {
-    method: 'GET',
-    headers: header,
-    redirect: 'follow'
+function getRequestOptions(apiKey: string): RequestInit {
+    const header = new Headers();
+    header.append('X-API-KEY', apiKey);
+    header.append('content-type', 'application/json');
+    return {
+        method: 'GET',
+        headers: header,
+        redirect: 'follow'
+    }
 }
+const defaultBaseUrl = 'https://opendata.resas-portal.go.jp/api/v1/';
 
-export async function getPrefectures(): Promise<PrefectureResponseObject> {
-    const response = (await fetch(createUrl('prefectures/'), requestOptions));
+export async function getPrefectures(apiKey: string, baseUrl: string = defaultBaseUrl): Promise<PrefectureResponseObject> {
+    const response = await fetch(
+        createUrl(baseUrl, 'prefectures/'), 
+        getRequestOptions(apiKey)
+    );
     return prefectureResponseObjectSchema.parseAsync(await response.json());
 }
 
-export async function getCompositionPopulation(prefCode: number, cityCode: number | '-', addArea?: string): Promise<PopulationResponseObject> {
-    const response = await fetch(createUrl('population/composition/perYear/', new URLSearchParams({prefCode: `${prefCode}`, cityCode: `${cityCode}`, addArea: addArea??''})), requestOptions);
+export async function getCompositionPopulation(apiKey: string, prefCode: number, cityCode: number | '-', baseUrl: string = defaultBaseUrl, addArea?: string): Promise<PopulationResponseObject> {
+    const response = await fetch(
+        createUrl(
+            baseUrl,
+            'population/composition/perYear/', 
+            new URLSearchParams({
+                prefCode: `${prefCode}`, 
+                cityCode: `${cityCode}`, 
+                addArea: addArea??''
+            })), 
+        getRequestOptions(apiKey)
+    );
     return populationResponseObjectSchema.parseAsync(await response.json());
 }
