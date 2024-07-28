17c38ba15b929ecda3bdca998595cc985311ffc9
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-29 04:45:43 +0900

    docs: jsdocを追加

--- packages/resas-api-wrapper/src/lib/fetch.ts
+++ packages/resas-api-wrapper/src/lib/fetch.ts
@@ -1,7 +1,11 @@
 import { createUrl } from '../utilities/endpointUtil';
-import envUtil from '../utilities/envUtil';
 import { PopulationResponseObject, populationResponseObjectSchema, PrefectureResponseObject, prefectureResponseObjectSchema } from './types';
 
+/**
+ * RESAS APIにリクエストするために必要なオプションを作成する関数
+ * @param apiKey 
+ * @returns {RequestInit}
+ */
 function getRequestOptions(apiKey: string): RequestInit {
     const header = new Headers();
     header.append('X-API-KEY', apiKey);
@@ -12,8 +16,16 @@ function getRequestOptions(apiKey: string): RequestInit {
         redirect: 'follow'
     }
 }
+
 const defaultBaseUrl = 'https://opendata.resas-portal.go.jp/api/v1/';
 
+/**
+ * RESAS APIの都道府県一覧を取得するための関数
+ * reference: https://opendata.resas-portal.go.jp/docs/api/v1/prefectures.html
+ * @param apiKey 
+ * @param baseUrl 
+ * @returns {Promise<PrefectureResponseObject>}
+ */
 export async function getPrefectures(apiKey: string, baseUrl: string = defaultBaseUrl): Promise<PrefectureResponseObject> {
     const response = await fetch(
         createUrl(baseUrl, 'prefectures/'), 
@@ -22,6 +34,16 @@ export async function getPrefectures(apiKey: string, baseUrl: string = defaultBa
     return prefectureResponseObjectSchema.parseAsync(await response.json());
 }
 
+/**
+ * RESAS APIの人口構成を取得するための関数
+ * reference: https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+ * @param apiKey 
+ * @param prefCode 
+ * @param cityCode 
+ * @param baseUrl 
+ * @param addArea 
+ * @returns {Promise<PopulationResponseObject>}
+ */
 export async function getCompositionPopulation(apiKey: string, prefCode: number, cityCode: number | '-', baseUrl: string = defaultBaseUrl, addArea?: string): Promise<PopulationResponseObject> {
     const response = await fetch(
         createUrl(
