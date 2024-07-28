7995f96e53601b7ae23cd738d1bab55816977e63
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-28 23:45:05 +0900

    update: APIKeyが見つからない時エラーとして処理するよう記述

--- packages/resas-api-wrapper/src/utilities/envUtil.ts
+++ packages/resas-api-wrapper/src/utilities/envUtil.ts
@@ -1,8 +1,10 @@
 export default {
     get baseUrl(): string {
-        return process.env.RESAS_API_BASE_URL || 'https://opendata.resas-portal.go.jp';
+        return process.env.RESAS_API_BASE_URL ?? 'https://opendata.resas-portal.go.jp/api/v1/';
     },
     get apiKey(): string {
-        return process.env.RESAS_API_KEY || '';
+        const key = process.env.RESAS_API_KEY;
+        if (!key) throw Error('API_KEY is undefined');
+        return key;
     }
 }
