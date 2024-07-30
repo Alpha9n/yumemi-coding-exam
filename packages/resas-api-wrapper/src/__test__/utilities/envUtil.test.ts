38dfcd9566fa1b7e2bf86b1d39869c0f12c78197
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-07-30 19:19:17 +0900

    refactor: パッケージ側でのenv読み込みが必要なくなったため、テストと同時に削除

--- packages/resas-api-wrapper/src/__test__/utilities/envUtil.test.ts
+++
@@ -1,9 +0,0 @@
-import env from '../../utilities/envUtil';
-
-describe('環境変数ユーティリティ', () => {
-    test('環境変数をstringで取得できる', () => {
-        const baseUrl = env.baseUrl;
-        const apiKey = env.apiKey;
-        expectTypeOf({ url: baseUrl, key: apiKey}).toEqualTypeOf<{ url: string, key: string }>();
-    });
-});
