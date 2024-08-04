e26c80b3bc08a59edef9704a96a8dcce0fe35605
Alpha9n <42644369+Alpha9n@users.noreply.github.com>
2024-08-04 19:47:09 +0900

    test: 導入するリストの型を変更

--- packages/population-trend-ui/src/__test__/components/molecules/MultipleCheck.test.tsx
+++ packages/population-trend-ui/src/__test__/components/molecules/MultipleCheck.test.tsx
@@ -4,18 +4,19 @@ import React from 'react';
 import { setup } from '../../utils/testUtils';
 import { screen } from '@testing-library/react';
 
-const checkBoxList = [{prefCode: 1, prefName: "北海道"}, {prefCode: 2, prefName: "青森県"}, {prefCode: 3, prefName: "岩手県"}];
+const prefList = [{prefCode: 1, prefName: "北海道"}, {prefCode: 2, prefName: "青森県"}, {prefCode: 3, prefName: "岩手県"}];
+const insertList = prefList.map((value) => ({id: value.prefCode.toString(), name: value.prefName}));
 const title = 'タイトル';
 
 describe('MultipleCheck テスト', () => {
     test('タイトルがpropsの通りに設定されている', async () => {
-        setup(<MultipleCheck title={title} selectList={checkBoxList}/>);
+        setup(<MultipleCheck title={title} selectList={insertList}/>);
         const target = await screen.findByRole('heading', {level: 2});
-        expect(target).toBe(title);
+        expect(target.innerHTML).toBe(title);
     });
 
     test('チェックボックスのリストがpropsで渡した数展開されている', async () => {
-        setup(<MultipleCheck title={title} selectList={checkBoxList}/>);
+        setup(<MultipleCheck title={title} selectList={insertList}/>);
         const target = await screen.findAllByRole('checkbox');
         expect(target.length).toBe(3);
     });
